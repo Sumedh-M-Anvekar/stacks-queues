61dc23eee3c5d9b8c3824678a0a0d45072a3692c
Sumedh M Anvekar <sumedhm8888@gmail.com>
2023-08-22 21:44:48 +0530

    stack implementation using arrays

---
+++ stackop.cpp
@@ -0,0 +1,81 @@
+#include<stdio.h>
+using namespace std;
+
+class stack{
+    public:
+        int top;
+        int *arr;
+        int size;
+    
+    stack(int size)
+    {
+        top=-1;
+        arr=new int[size];
+    }
+    void push(int element)
+    {
+        if(size-top>=1)
+        {
+            top++;
+            arr[top]=element;
+        
+        }
+        else
+        {
+            cout<<"stack overflow yaar"<<endl;
+        }
+    }
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"stack underflow yaar"<<endl;
+        }
+        else
+        {
+            top--;
+        }
+    }
+    int topp()
+    {
+        if(top>=0)
+        {
+            return arr[top];
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    bool isempty()
+    {
+        if(top==-1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    stack st(5);
+    st.push(88);
+    st.push(99);
+    st.push(77);
+    st.push(123);
+    st.push(8);
+
+    //pop
+    st.pop();
+    st.pop();
+
+    //top of stack
+    st.topp();
+
+    //isempty'
+    st.isempty();
+}
